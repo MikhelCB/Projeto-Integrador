fb2a4ebecff338b504578a6c8a95ebf7782c589f
“MikhelCB” <srrynow@hotmail.com>
2026-03-11 21:23:51 -0300

    feat: created route /usuarios, create routes.js, method post, get and delete and create usercontrollers to do contact with data base

---
+++ src/controllers/userController.js
@@ -0,0 +1,11 @@
+export const createUser = (req, res) => {
+  res.status(200).json({ message: "Deu bom" });
+};
+
+export const getAllUsers = (req, res) => {
+  res.status(200).json({ message: "Deu bom" });
+};
+
+export const deleteUser = (req, res) => {
+  res.status(200).json({ message: "Deu bom" });
+};
