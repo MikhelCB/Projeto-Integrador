549aa31d9225b62b8821e22aaaf870fa4dbf1bf5
Mikhel <mikhelcabral22@gmail.com>
2026-03-31 11:22:28 -0300

    feat: added init all of models in server.js and make relationships about tutor, animal, servico, agendamento and conecting with data base

--- src/server.js
+++ src/server.js
@@ -1,9 +1,37 @@
 import express from "express";
+import Tutor from "./models/Tutor.js";
+import Animal from "./models/Animal.js";
+import Servico from "./models/Servico.js";
+import Agendamento from "./models/Agendamento.js";
+
+import { Sequelize } from "sequelize";
+import config from "./config/database.js";
 import userRoutes from "./routes.js";
 
 const app = express();
 app.use(express.json());
 
+const sequelize = new Sequelize(config);
+//Fazendo o init de todos os models
+Tutor.init(sequelize);
+Animal.init(sequelize);
+Servico.init(sequelize);
+Agendamento.init(sequelize);
+
+//Fazendo os relacionamentos
+Tutor.associate({ Animal, Agendamento });
+Animal.associate({ Tutor, Agendamento });
+Servico.associate({ Agendamento });
+Agendamento.associate({ Tutor, Animal, Servico });
+
 app.use("/usuarios", userRoutes);
 
-app.listen(3000, () => console.log("Servidor ON"));
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Banco de dados conectado");
+    app.listen(3000, () => console.log("Servidor ON"));
+  })
+  .catch((err) => {
+    console.error(err);
+  });
