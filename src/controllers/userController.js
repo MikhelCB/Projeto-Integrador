7a2a8fa18aa9c0482faf84dec0b4b36e714ffa3d
Mikhel <mikhelcabral22@gmail.com>
2026-04-01 19:42:32 -0300

    fix: removing lib crypto

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,16 +1,19 @@
 import Tutor from "../models/Tutor.js";
-import crypto from "node:crypto";
 
 export const createTutor = async (req, res) => {
-  const createTutor = {
-    nome: req.body.nome,
-    cpf: req.body.cpf,
-    telefone: req.body.telefone,
-    endereco: req.body.endereco,
-  };
-  const tutor = await Tutor.create(createTutor);
+  try {
+    const createTutor = {
+      nome: req.body.nome,
+      cpf: req.body.cpf,
+      telefone: req.body.telefone,
+      endereco: req.body.endereco,
+    };
+    const tutor = await Tutor.create(createTutor);
 
-  res.status(201).json(tutor);
+    res.status(201).json(tutor);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 export const getAllUsers = (req, res) => {
