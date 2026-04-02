ee4898d68dac1305e56ce0752dfd30e593fbfd65
Mikhel <mikhelcabral22@gmail.com>
2026-04-02 10:21:40 -0300

    feat: create .env and hide the data of database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+import "dotenv/config.js";
+
 export default {
-  dialect: "postgres",
-  host: "localhost",
-  username: "postgres",
-  password: "postgres",
-  database: "usersdb",
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   define: {
     timestamp: true,
     underscored: true,
